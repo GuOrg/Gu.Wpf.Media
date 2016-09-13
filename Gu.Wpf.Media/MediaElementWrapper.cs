e34da80843cb11ceab04ca00ad2486449857b823
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-13 11:57:43 +0200

    Move dependency property bloat to a partial class.

--- Gu.Wpf.Media/MediaElementWrapper.cs
+++ Gu.Wpf.Media/MediaElementWrapper.cs
@@ -7,53 +7,10 @@
     using System.Windows.Input;
     using System.Windows.Threading;
 
-    public class MediaElementWrapper : Decorator
+    public partial class MediaElementWrapper : Decorator
     {
-        public static readonly DependencyProperty SourceProperty = MediaElement.SourceProperty.AddOwner(typeof(MediaElementWrapper));
-
-        public static readonly DependencyProperty StateProperty = DependencyProperty.Register(
-            nameof(State),
-            typeof(MediaState),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata(default(MediaState), OnStateChanged));
-
-        public static readonly DependencyProperty PositionProperty = DependencyProperty.Register(
-            nameof(Position),
-            typeof(TimeSpan?),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata(default(TimeSpan?), OnPositionChanged));
-
-        private static readonly DependencyPropertyKey LengthPropertyKey = DependencyProperty.RegisterReadOnly(
-            "Length",
-            typeof(TimeSpan?),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata(default(TimeSpan?)));
-
-        public static readonly DependencyProperty VolumeProperty = MediaElement.VolumeProperty.AddOwner(typeof(MediaElementWrapper));
-
-        public static readonly DependencyProperty VolumeIncrementProperty = DependencyProperty.Register(
-            nameof(VolumeIncrement),
-            typeof(double),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata(default(double)));
-
-        public static readonly DependencyProperty VideoFormatsProperty = DependencyProperty.Register(
-            nameof(VideoFormats),
-            typeof(string),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata("*.dat; *.wmv; *.3g2; *.3gp; *.3gp2; *.3gpp; *.amv; *.asf;  *.avi; *.bin; *.cue; *.divx; *.dv; *.flv; *.gxf; *.iso; *.m1v; *.m2v; *.m2t; *.m2ts; *.m4v; *.mkv; *.mov; *.mp2; *.mp2v; *.mp4; *.mp4v; *.mpa; *.mpe; *.mpeg; *.mpeg1; *.mpeg2; *.mpeg4; *.mpg; *.mpv2; *.mts; *.nsv; *.nuv; *.ogg; *.ogm; *.ogv; *.ogx; *.ps; *.rec; *.rm; *.rmvb; *.tod; *.ts; *.tts; *.vob; *.vro; *.webm"));
-
-        // https://support.microsoft.com/en-us/kb/316992
-        public static readonly DependencyProperty AudioFormatsProperty = DependencyProperty.Register(
-            nameof(AudioFormats),
-            typeof(string),
-            typeof(MediaElementWrapper),
-            new PropertyMetadata("*.mp3; *.wma; *.aac; *.adt; *.adts; *.m4a; *.wav; *.aif; *.aifc; *.aiff; *.cda"));
-
-        public static readonly DependencyProperty LengthProperty = LengthPropertyKey.DependencyProperty;
-
         private readonly DispatcherTimer updatePositionTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.1) };
-        private MediaElement mediaElement;
+        private readonly MediaElement mediaElement;
 
         public MediaElementWrapper()
         {
@@ -81,69 +38,6 @@
             this.CommandBindings.Add(new CommandBinding(MediaCommands.IncreaseVolume, HandleExecute(() => this.IncreaseVolume(this.VolumeIncrement)), HandleCanExecute(this.CanIncreaseVolume)));
             this.CommandBindings.Add(new CommandBinding(MediaCommands.DecreaseVolume, HandleExecute(() => this.DecreaseVolume(this.VolumeIncrement)), HandleCanExecute(this.CanDecreaseVolume)));
             this.updatePositionTimer.Tick += (o, e) => this.Position = this.mediaElement?.Position;
-        }
-
-        /// <summary>
-        /// See <see cref="MediaElement.Source"/>
-        /// </summary>
-        public Uri Source
-        {
-            get { return (Uri)this.GetValue(SourceProperty); }
-            set { this.SetValue(SourceProperty, value); }
-        }
-
-        public MediaState State
-        {
-            get { return (MediaState)this.GetValue(StateProperty); }
-            set { this.SetValue(StateProperty, value); }
-        }
-
-        /// <summary>
-        /// The position in the current media.
-        /// </summary>
-        public TimeSpan? Position
-        {
-            get { return (TimeSpan?)this.GetValue(PositionProperty); }
-            set { this.SetValue(PositionProperty, value); }
-        }
-
-        /// <summary>
-        /// The length of the current media
-        /// </summary>
-        public TimeSpan? Length
-        {
-            get { return (TimeSpan?)this.GetValue(LengthProperty); }
-            protected set { this.SetValue(LengthPropertyKey, value); }
-        }
-
-        /// <summary>
-        /// See <see cref="MediaElement.Volume"/>
-        /// </summary>
-        public double Volume
-        {
-            get { return (double)this.GetValue(VolumeProperty); }
-            set { this.SetValue(VolumeProperty, value); }
-        }
-
-        /// <summary>
-        /// The increment by which the IncreaseVolume
-        /// </summary>
-        public double VolumeIncrement
-        {
-            get { return (double)this.GetValue(VolumeIncrementProperty); }
-            set { this.SetValue(VolumeIncrementProperty, value); }
-        }
-
-        public string VideoFormats
-        {
-            get { return (string)this.GetValue(VideoFormatsProperty); }
-            set { this.SetValue(VideoFormatsProperty, value); }
-        }
-
-        public string AudioFormats
-        {
-            get { return (string)this.GetValue(AudioFormatsProperty); }
-            set { this.SetValue(AudioFormatsProperty, value); }
         }
 
         public override void BeginInit()

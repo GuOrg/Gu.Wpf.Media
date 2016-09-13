a659e35c08486a5838fc4b120e5d212bd3aca779
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-13 17:40:40 +0200

    FEATURE: Commands.ToggleFullScreen

---
+++ Gu.Wpf.Media/Commands.cs
@@ -0,0 +1,18 @@
+﻿namespace Gu.Wpf.Media
+{
+    using System.Windows.Input;
+
+    /// <summary>
+    /// A set of Standard Commands
+    /// </summary>
+    public static class Commands
+    {
+        /// <summary>
+        /// Toggle full screen command.
+        /// </summary>
+        public static RoutedUICommand ToggleFullScreen { get; } = new RoutedUICommand(
+                                                                      "Toggle full screen.",
+                                                                      nameof(ToggleFullScreen),
+                                                                      typeof(Commands));
+    }
+}

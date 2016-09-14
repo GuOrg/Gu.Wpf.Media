e65011949479932de0435b8176c253385c06c8d7
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-14 17:01:19 +0200

    NEW FEATURE: Drag.PauseWhileDragging
    Fixed icons for toggle button. Thanks @Venomed

--- Gu.Wpf.Media.Demo/MainWindow.xaml.cs
+++ Gu.Wpf.Media.Demo/MainWindow.xaml.cs
@@ -2,16 +2,12 @@
 {
     using System;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Controls.Primitives;
     using System.Windows.Input;
 
     using Microsoft.Win32;
 
     public partial class MainWindow : Window
     {
-        private MediaState mediaState;
-
         public MainWindow()
         {
             this.InitializeComponent();
@@ -27,20 +23,6 @@
             if (openFileDialog.ShowDialog() == true)
             {
                 this.MediaElement.Source = new Uri(openFileDialog.FileName);
-            }
-        }
-
-        private void OnProgressSliderDragStarted(object sender, DragStartedEventArgs e)
-        {
-            this.mediaState = this.MediaElement.State;
-            this.MediaElement.Pause();
-        }
-
-        private void OnProgressSliderDragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            if (this.mediaState == MediaState.Play)
-            {
-                this.MediaElement.Play();
             }
         }
 

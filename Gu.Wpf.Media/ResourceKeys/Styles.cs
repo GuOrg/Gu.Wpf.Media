d32da6f3c0d37a8092caa4e05a4f8196d7df0bb3
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-14 16:20:17 +0200

    Move PlayerButtonBaseStyle to lib.

---
+++ Gu.Wpf.Media/ResourceKeys/Styles.cs
@@ -0,0 +1,14 @@
+﻿namespace Gu.Wpf.Media
+{
+    using System.Windows;
+
+    public static class Styles
+    {
+        public static ResourceKey PlayerButtonBaseStyleKey { get; } = Create(nameof(PlayerButtonBaseStyleKey));
+
+        private static ComponentResourceKey Create(string name)
+        {
+            return new ComponentResourceKey(typeof(Geometries), name.Replace("Key", string.Empty));
+        }
+    }
+}

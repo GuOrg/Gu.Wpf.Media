ed292c6a423c8ab9f9a990f61cee9ca389d9c5f6
JohanLarsson <johan_gavle1@hotmail.com>
2016-09-19 02:12:16 +0200

    Define brushes for enabled & disabled.

---
+++ Gu.Wpf.Media/ResourceKeys/Brushes.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable SA1600
+#pragma warning disable 1591
+namespace Gu.Wpf.Media
+{
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+
+    /// <summary>
+    /// A set of resource keys for brushes.
+    /// </summary>
+    public static class Brushes
+    {
+        public static ResourceKey EnabledForegroundBrushKey { get; } = Create();
+
+        public static ResourceKey DisabledForegroundBrushKey { get; } = Create();
+
+        private static ComponentResourceKey Create([CallerMemberName]string name = null)
+        {
+            return new ComponentResourceKey(typeof(Brushes), name.TrimEnd("Key"));
+        }
+    }
+}

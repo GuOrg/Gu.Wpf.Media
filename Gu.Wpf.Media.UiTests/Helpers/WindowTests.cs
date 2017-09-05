5899b671ceb130804689dad3c4db2efd055faf7a
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-05 13:42:44 +0200

    Refactor

--- Gu.Wpf.Media.UiTests/Helpers/WindowTests.cs
+++ Gu.Wpf.Media.UiTests/Helpers/WindowTests.cs
@@ -1,23 +1,28 @@
 namespace Gu.Wpf.Media.UiTests.Helpers
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
     using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
+    using Application = Gu.Wpf.UiAutomation.Application;
+    using Window = Gu.Wpf.UiAutomation.Window;
 
     public abstract class WindowTests : IDisposable
     {
+        private readonly ConcurrentDictionary<string, AutomationElement> itemCache = new ConcurrentDictionary<string, AutomationElement>();
         private Application application;
         private bool disposed;
 
-        public static Window StaticWindow { get; private set; }
-
-        protected Window Window => StaticWindow;
+        protected Window Window => this.application.MainWindow;
 
         protected abstract string WindowName { get; }
 
         public virtual void RestartApplication()
         {
             this.OneTimeTearDown();
+            this.itemCache.Clear();
             this.OneTimeSetUp();
         }
 
@@ -26,7 +31,6 @@ namespace Gu.Wpf.Media.UiTests.Helpers
         {
             this.application?.Dispose();
             this.application = Application.AttachOrLaunch(Info.CreateStartInfo(this.WindowName));
-            StaticWindow = this.application.MainWindow;
             ////this.SaveScreenshotToArtifactsDir("start");
         }
 
@@ -35,7 +39,53 @@ namespace Gu.Wpf.Media.UiTests.Helpers
         {
             ////this.SaveScreenshotToArtifactsDir("finish");
             this.application?.Dispose();
-            StaticWindow = null;
+        }
+
+        public void AssertReadOnly(bool expected, DependencyProperty property)
+        {
+            var textBox = this.GetCachedTextBox(property.Name);
+            var readonlyText = expected
+                ? "readonly"
+                : "editable";
+            Assert.AreEqual(expected, !textBox.IsEnabled, $"Expected the property {property.Name} to be {readonlyText}");
+        }
+
+        public void AreEqual(string expected, DependencyProperty property)
+        {
+            var valueBox = this.GetCachedTextBox(property.Name);
+            Assert.AreEqual(expected, valueBox.Text, $"Expected the value of {property.Name} to be {expected} was: {valueBox.Text}");
+        }
+
+        public TextBox GetCachedTextBox(string name)
+        {
+            return (TextBox)this.itemCache.GetOrAdd(name, n => this.Window.FindTextBox(n));
+        }
+
+        public Button GetCachedButton([CallerMemberName]string name = null)
+        {
+            Assert.NotNull(name);
+            name = name.EndsWith("Button") ? name.TrimEnd("Button") : name;
+            return (Button)this.itemCache.GetOrAdd(name, n => this.Window.FindButton(n));
+        }
+
+        public void SetValue(DependencyProperty property, string value)
+        {
+            var valueBox = this.GetCachedTextBox(property.Name);
+
+            if (valueBox.Text != value)
+            {
+                valueBox.Text = value;
+                ////this.Window.WaitWhileBusy();
+                //// ReSharper disable once ExplicitCallerInfoArgument
+                this.GetCachedButton("Lose focus")
+                    .Click();
+            }
+        }
+
+        public string GetValue(DependencyProperty property)
+        {
+            var valueBox = this.GetCachedTextBox(property.Name);
+            return valueBox.Text;
         }
 
         public void Dispose()
--- Gu.Wpf.Media.UiTests/TestPlayerWindowTests.cs
+++ Gu.Wpf.Media.UiTests/TestPlayerWindowTests.cs
@@ -1,17 +1,10 @@
 namespace Gu.Wpf.Media.UiTests
 {
-    using System.Collections.Concurrent;
-    using System.Runtime.CompilerServices;
-    using System.Windows;
-
     using Gu.Wpf.Media.UiTests.Helpers;
-    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
     public partial class TestPlayerWindowTests : WindowTests
     {
-        private readonly ConcurrentDictionary<string, AutomationElement> itemCache = new ConcurrentDictionary<string, AutomationElement>();
-
         protected override string WindowName { get; } = "TestPlayerWindow";
 
         [SetUp]
@@ -22,59 +15,6 @@ namespace Gu.Wpf.Media.UiTests
             this.SetValue(MediaElementWrapper.SourceProperty, string.Empty);
             this.SetValue(MediaElementWrapper.VolumeProperty, "0.5");
             this.SetValue(MediaElementWrapper.VolumeIncrementProperty, "0.05");
-        }
-
-        public override void RestartApplication()
-        {
-            this.itemCache.Clear();
-            base.RestartApplication();
-        }
-
-        public void AssertReadOnly(bool expected, DependencyProperty property)
-        {
-            var textBox = this.GetCachedTextBox(property.Name);
-            var readonlyText = expected
-                ? "readonly"
-                : "editable";
-            Assert.AreEqual(expected, !textBox.IsEnabled, $"Expected the property {property.Name} to be {readonlyText}");
-        }
-
-        public void AreEqual(string expected, DependencyProperty property)
-        {
-            var valueBox = this.GetCachedTextBox(property.Name);
-            Assert.AreEqual(expected, valueBox.Text, $"Expected the value of {property.Name} to be {expected} was: {valueBox.Text}");
-        }
-
-        public TextBox GetCachedTextBox(string name)
-        {
-            return (TextBox)this.itemCache.GetOrAdd(name, n => this.Window.FindTextBox(n));
-        }
-
-        public Button GetCachedButton([CallerMemberName]string name = null)
-        {
-            Assert.NotNull(name);
-            name = name.EndsWith("Button") ? name.TrimEnd("Button") : name;
-            return (Button)this.itemCache.GetOrAdd(name, n => this.Window.FindButton(n));
-        }
-
-        private void SetValue(DependencyProperty property, string value)
-        {
-            var valueBox = this.GetCachedTextBox(property.Name);
-
-            if (valueBox.Text != value)
-            {
-                valueBox.Text = value;
-                ////this.Window.WaitWhileBusy();
-                //// ReSharper disable once ExplicitCallerInfoArgument
-                this.GetCachedButton("Lose focus")
-                    .Click();
-            }
-        }
-
-        private string GetValue(DependencyProperty property)
-        {
-            var valueBox = this.GetCachedTextBox(property.Name);
-            return valueBox.Text;
         }
     }
 }

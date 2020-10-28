92378bd3fa28792b3b104daea15200ae24a434f9
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-28 19:16:33 +0100

    Use new csproj and nuget

---
+++ Gu.Wpf.Media/AssemblyAttributes.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.Media", "Gu.Wpf.Media")]
+[assembly: XmlnsPrefix("https://github.com/JohanLarsson/Gu.Wpf.Media", "media")]
+[assembly: InternalsVisibleTo("Gu.Wpf.Media.Tests")]
+[assembly: InternalsVisibleTo("Gu.Wpf.Media.UiTests")]

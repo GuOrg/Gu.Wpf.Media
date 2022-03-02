09193e2f17ac498813b64aeac1d6583a7e654dd2
JohanLarsson <johan_gavle1@hotmail.com>
2022-03-02 14:27:23 +0100

    Use Assembly.Location

--- Gu.Wpf.Media.Demo/UiTestWindows/Files.cs
+++ Gu.Wpf.Media.Demo/UiTestWindows/Files.cs
@@ -1,21 +1,11 @@
-﻿namespace Gu.Wpf.Media.Demo.UiTestWindows
+namespace Gu.Wpf.Media.Demo.UiTestWindows
 {
-    using System;
     using System.IO;
     using System.Reflection;
 
     public static class Files
     {
-        public static string AssemblyDirectory
-        {
-            get
-            {
-                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                var uri = new UriBuilder(codeBase);
-                var path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path);
-            }
-        }
+        public static string AssemblyDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location!)!;
 
         public static string CoffeeClip { get; } = Path.Combine(AssemblyDirectory, "Samples", "coffee.mp4");
     }
